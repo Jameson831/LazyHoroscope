1abfaf70b21d6651b0ca1f28381c96267e01fad7
Jameson831 <jtwhite@mail.csuchico.edu>
2019-09-26 17:12:22 -0700

    Lucky Number

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 //Authors:
 #include<iostream>
-#include<string>
+
 
 using namespace std;
 
-int main()
-{
-  //declare variables
-
-  //get user input
-
-  //tell fortune
-
-  return 0;
+int main(){
+        string firstName;
+        string lastName;
+        int lucky = 0;
+  
+cout <<  "What is your first name? "<<endl;
+cin>> firstName;
+cout<< "What is your last name? ";
+cin>> lastName;
+cout<< "Welcome " <<firstName.at(0) <<"."  <<lastName.at(0) <<" ."<< " , here is your fortune... " ;
+ for(int i=0; firstName[i]; i++){
+ lucky++;
+ }
+cout << "your lucky number is " << lucky << endl;
+return 0;
 }
