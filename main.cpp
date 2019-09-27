72c5ddbf5bf33d20bdf0a3638c8d5d7f730c5d1e
Jameson831 <jtwhite@mail.csuchico.edu>
2019-09-27 15:48:58 -0700

    My lazyHoroscope

--- main.cpp
+++ main.cpp
@@ -8,15 +8,34 @@ int main(){
         string firstName;
         string lastName;
         int lucky = 0;
-  
-cout <<  "What is your first name? "<<endl;
-cin>> firstName;
-cout<< "What is your last name? ";
-cin>> lastName;
-cout<< "Welcome " <<firstName.at(0) <<"."  <<lastName.at(0) <<" ."<< " , here is your fortune... " ;
+        int lastLetter;
+    cout <<  "What is your first name? "<<endl;
+    cin>> firstName;
+    cout<< "What is your last name? ";
+    cin>> lastName;
+    cout<< "Welcome " <<firstName.at(0) <<"."  <<lastName.at(0) <<" ."<< " , here is your fortune... " ;
  for(int i=0; firstName[i]; i++){
- lucky++;
- }
+    lucky++;
+    }
 cout << "your lucky number is " << lucky << endl;
+if (firstName[0] == 'a' || firstName [0] == 'A' ||firstName[0] == 'e' || firstName[0] == 'E' ||
+    firstName[0] == 'I' || firstName[0] == 'i' || firstName[0] == 'o' || firstName[0] == 'O' || 
+    firstName[0] == 'u' || firstName[0] == 'U') {
+    cout<< "You are destined to be famous!" <<endl;
+    }
+else{
+    cout << "You Should keep a low profile" << endl;
+    } 
+ for (int i =0; lastName[i]; ++i){ 
+     lastLetter = i;
+     }
+if (lastName[lastLetter]== 'a' || lastName[lastLetter] == 'A' || lastName[lastLetter] == 'e' ||
+     lastName[lastLetter] == 'E' || lastName[lastLetter] == 'o' || lastName[lastLetter] == 'O' || 
+     lastName[lastLetter]== 'i' || lastName[lastLetter] == 'I' || lastName[lastLetter] == 'U' ||
+     lastName[lastLetter] == 'u' ){
+    cout<< "You have already met your true love." <<endl;
+    }
+cout << "Have a good day!"<<endl; 
+
 return 0;
 }
