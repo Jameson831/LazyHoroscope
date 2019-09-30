796670db9748ee0352cd7c80e2879d46045772f3
Jameson831 <jtwhite@mail.csuchico.edu>
2019-09-30 15:41:15 -0700

    Lucky New Horoscope 2

--- main.cpp
+++ main.cpp
@@ -13,18 +13,18 @@ int main(){
     cin>> firstName;
     cout<< "What is your last name? "<<endl;
     cin>> lastName;
-    cout<< "Welcome " <<firstName.at(0) <<"."  <<lastName.at(0) <<" ."<< " , here is your fortune... " ;
+    cout<< "Welcome " <<firstName.at(0) <<"."  <<lastName.at(0) <<" ."<< " , here is your fortune... "<< endl;
  for(int i=0; firstName[i]; i++){
     lucky++;
     }
-cout << "your lucky number is " << lucky << endl;
+cout << "your lucky number is "  << lucky<< endl;
 if (firstName[0] == 'a' || firstName [0] == 'A' ||firstName[0] == 'e' || firstName[0] == 'E' ||
     firstName[0] == 'I' || firstName[0] == 'i' || firstName[0] == 'o' || firstName[0] == 'O' || 
     firstName[0] == 'u' || firstName[0] == 'U') {
     cout<< "You are destined to be famous!" <<endl;
     }
 else{
-    cout << "You Should keep a low profile" << endl;
+    cout << "You Should keep a low profile." << endl;
     } 
  for (int i =0; lastName[i]; ++i){ 
      lastLetter = i;
