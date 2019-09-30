ae08842dda7e7f7226b28701604fc0d953c8d8e9
Jameson831 <jtwhite@mail.csuchico.edu>
2019-09-30 15:45:40 -0700

    Lucky New Horoscope 2

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(){
     cin>> firstName;
     cout<< "What is your last name? "<<endl;
     cin>> lastName;
-    cout<< "Welcome " <<firstName.at(0) <<"."  <<lastName.at(0) <<" ."<< " , here is your fortune... "<< endl;
+    cout<< "Welcome, " <<firstName.at(0) <<"."  <<lastName.at(0) <<"."<< ", here is your fortune... "<< endl;
  for(int i=0; firstName[i]; i++){
     lucky++;
     }
@@ -21,10 +21,10 @@ cout << "your lucky number is "  << lucky<< endl;
 if (firstName[0] == 'a' || firstName [0] == 'A' ||firstName[0] == 'e' || firstName[0] == 'E' ||
     firstName[0] == 'I' || firstName[0] == 'i' || firstName[0] == 'o' || firstName[0] == 'O' || 
     firstName[0] == 'u' || firstName[0] == 'U') {
-    cout<< "You are destined to be famous!" <<endl;
+    cout<< "you are destined to be famous!" <<endl;
     }
 else{
-    cout << "You Should keep a low profile." << endl;
+    cout << "you Should keep a low profile." << endl;
     } 
  for (int i =0; lastName[i]; ++i){ 
      lastLetter = i;
@@ -33,7 +33,7 @@ if (lastName[lastLetter]== 'a' || lastName[lastLetter] == 'A' || lastName[lastLe
      lastName[lastLetter] == 'E' || lastName[lastLetter] == 'o' || lastName[lastLetter] == 'O' || 
      lastName[lastLetter]== 'i' || lastName[lastLetter] == 'I' || lastName[lastLetter] == 'U' ||
      lastName[lastLetter] == 'u' ){
-    cout<< "You have already met your true love." <<endl;
+    cout<< "you have already met your true love." <<endl;
     }
 cout << "Have a good day!"<<endl; 
 
